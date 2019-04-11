6a23f7487298148cccf15b4b7a36e21aa3ffd409
arqdsis <arqdsis@MOOLI10306.animaedu.intranet>
2019-04-10 21:31:43 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ciclodevidaegps/APPConstants.java
@@ -0,0 +1,10 @@
+/**
+ * Uriel Henrique Marques Farias Mishima RA: 81717300
+ */
+
+package br.usjt.ciclodevidaegps;
+
+public class APPConstants {
+    public static final long REQUEST_LOCATION_UPDATE_MIN_TIME = 0;
+    public static final long REQUEST_LOCATION_UPDATE_MIN_DISTANCE = 0;
+}
---
+++ app/src/main/java/br/usjt/ciclodevidaegps/Localizacao.java
@@ -0,0 +1,53 @@
+/**
+ * Uriel Henrique Marques Farias Mishima RA: 81717300
+ */
+
+package br.usjt.ciclodevidaegps;
+
+import java.io.Serializable;
+
+public class Localizacao implements Serializable {
+
+    private int id;
+    private Double latitude;
+    private Double longitude;
+
+    public Localizacao(double latitude, double longitude){
+        this.latitude = longitude;
+        this.longitude = longitude;
+    }
+
+    public Localizacao(int id, double latitude, double longitude) {
+        this(latitude, longitude);
+        this.id = id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Lat: " + latitude + ", Long: " + longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
